f456188bd84bc09bb20ebb7aa47fabe3c93bea51
Yaska-Prog <christianleon442@gmail.com>
2024-07-06 15:19:46 +0700

    Navigation done

--- src/screens/Favorite.tsx
+++ src/screens/Favorite.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 export default function Favorite(): JSX.Element {
- return (
-   <View>
-     <Text>Favorite</Text>
-   </View>
- )
+    return (
+        <View>
+            <Text>Favorite</Text>
+        </View>
+    )
 }
